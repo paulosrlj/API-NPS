5468def87fc483ae57580567592c4e899870d709
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-07-02 21:07:14 -0300

    :sparkles: feat - Surveys routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import userController from './controllers/UserController';
+import surveysController from './controllers/SurveyController';
 
 const router = Router();
 
 router.post('/users', userController.create);
+
+router.get('/surveys', surveysController.show);
+router.post('/surveys', surveysController.create);
 
 export default router;
